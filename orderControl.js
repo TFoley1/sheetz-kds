ae08bd7413e2343c34981009c2926deddb8ca795
SFoleyIII <trudood05@gmail.com>
2026-03-16 15:03:28 -0400

    improvements to the order rendering

--- orderControl.js
+++ orderControl.js
@@ -1,24 +1,29 @@
 const activeOrders = [
     {
-    "itemName": "Burger",
-    "ingredients": ["Bun", "Mayo", "Burger", "American cheese"],
-    "orderNumber": "849",
-    "timer": "1:09",
-    "deliverTo":"Deliver To: Customer at Expo"
-    },
-    {
-    "itemName": "Street Tacos",
-    "ingredients": ["Tortilla", "Taco Beef", "Shredded Cheddar", "Lettuce"],
-    "orderNumber": "851",
-    "timer": "1:09",
-    "deliverTo":"Deliver To: Customer at Expo"
-    },
-    {
-    "itemName": "Burger",
-    "ingredients": ["bun", "mayo", "burger", "american cheese"],
     "orderNumber": "850",
     "timer": "1:09",
-    "deliverTo":"Deliver To: Customer at Expo"
+    "deliverTo":"Deliver To: Customer at Expo",
+    items: [
+        {
+        "itemName": "Burger",
+        "ingredients": ["bun", "mayo", "burger", "american cheese"]
+        },
+        {
+        "itemName": "Hot Dog",
+        "ingredients": ["Hot dog bun", "mmustard", "hot dog"]
+        }
+    ]
+    },
+    {
+    "orderNumber": "851",
+    "timer": "1:09",
+    "deliverTo":"Deliver To: Customer at Expo",
+    items: [
+        {
+        "itemName": "Burger",
+        "ingredients": ["bun", "mayo", "burger", "american cheese"]
+        }
+    ]
     }
 ]
 
@@ -34,15 +39,38 @@ const inactiveOrders = [
 
 // Take JSON and turn it into HTML & Tailwind
 function renderOrders() {
-
-  const container = $("#orders-container");
-  //container.classList.add("grid","grid-cols-2","gap-4","span-2");
-  container.innerHTML = ""; // clear before re-render
+    const container = $("#orders-container");
+    container.empty();
 
   activeOrders.forEach(order => {
-
     const orderCard = document.createElement("div");
     orderCard.classList.add("order","relative","border-3", "border-solid", "rounded-md", "col-span-2");
+
+    let itemsHTML = "";
+
+    // Loop through the items in whole order
+    order.items.forEach(item => {
+
+      let ingredientsHTML = "";
+
+      // LOOP THROUGH INGREDIENTS
+      item.ingredients.forEach(ingredient => {
+        ingredientsHTML += `<li>${ingredient}</li>`;
+      });
+
+      itemsHTML += `
+        <div class="flex-1">
+          <h2 class="text-lg font-bold border-dashed border-b-2 p-1">
+            ${item.itemName}
+          </h2>
+
+          <ul class="inline-block text-base p-2">
+            ${ingredientsHTML}
+          </ul>
+        </div>
+      `;
+
+    });
     orderCard.innerHTML = `
         <div class="grid grid-cols-3 gap-4 bg-green-700 border-b-3 p-2 ">
             <h3 class="text-xl font-bold">Order #${order.orderNumber}</h3>
@@ -50,20 +78,39 @@ function renderOrders() {
             <h3 class="text-sm font-bold text-center">${order.deliverTo}</h3>
         </div>
         <div class="flex flex-row gap-4">
-        <div class="flex-1">
-            <h2 class="text-lg font-bold border-dashed border-b-2 p-1">${order.itemName}</h2>
-            <ul class="inline-block text-base p-2">
-                ${order.ingredients.map(ingredient => `<li>${ingredient}</li>`).join("")}
-            </ul>
-        </div>
+            ${itemsHTML}
         </div>
     `;
     //container.appendChild(orderCard);
     $("#orders-container").append(orderCard);
   });
-
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   renderOrders();
 });
+
+let selectedOrder;
+
+
+$(document).ready(function(){
+
+    $("#bump-btn").click(function() {
+        // Selected Order gets saved to inactive order list at the beginning
+            // Find order based on #
+            // unshift or add to beginning
+            //inactiveOrders.unshift();
+        // Selected order gets removed from screen
+        selectedOrder.remove();
+    });
+
+    $(".order").click(function() {
+        $(".order").css("background-color","rgb(8,8,8)");
+        // Save order data into variable
+        // Highlight background
+        selectedOrder = $(this);
+        selectedOrder.css("background-color","rgb(30, 30, 30)");
+    });
+}); 
+
+
